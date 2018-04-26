13cc698b47e3638e39c354aa9ea9a84d849c8707
Mike Lothar <mikelothar@gmail.com>
2018-04-26 22:26:09 +0200

    probably about time i bumb the version

--- src/config/GameConfig.js
+++ src/config/GameConfig.js
@@ -1,12 +1,20 @@
 import Phaser from 'phaser'
 import Preloader from '../scenes/Preloader'
 import Main from '../scenes/Main'
+import { version } from '../../package.json'
 
 export default {
   type: Phaser.AUTO,
   parent: 'content',
   height: 19 * 35,
   width: 23 * 35,
+  title: 'Misimi',
+  url: 'http://mikelothar.github.io/misimi / http://misimi.netlify.com',
+  version,
+  roundPixels: true,
+  banner: {
+    hidePhaser: true,
+  },
   physics: {
     arcade: {
       debug: false,
